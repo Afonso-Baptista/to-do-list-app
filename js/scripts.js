04f45e85247faab105ea2268b3c9e30436d8534f
Afonso Baptista <afonso.7.baptista@gmail.com>
2022-01-20 16:54:29 +0000

    Refactor JS into jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,33 @@
 function newItem() {
 
     //1. Explanation: Adding a new item to the list of items:
-    let li = document.createElement('li');
-    let inputValue = document.getElementById('input').value;
-    let text = document.createTextNode(inputValue);
-    li.appendChild(text);
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
 
     if (inputValue === '') {
         alert('An empty item makes for an empty list!');
     } else {
-        let list = document.querySelector('#list');
-        list.appendChild(li);
+        $('#list').append(li);
     }
 
     //2. Explanation: Crossing out an item from the list of items:
     function crossOut() {
-        li.classList.toggle('strike');
+        li.toggleClass('strike');
     }
 
-    li.addEventListener('dblclick', crossOut);
+    li.on('dblclick', crossOut);
 
     //3(i). Explanation: Adding the delete button 'X':
-    let deleteButton = document.createElement('deleteButton');
-    deleteButton.appendChild(document.createTextNode('X'));
-    li.appendChild(deleteButton);
+    let deleteButton = $('<deleteButton></deleteButton>');
+    deleteButton.append(document.createTextNode('X'));
+    li.append(deleteButton);
 
-    deleteButton.addEventListener('click', deleteListItem);
+    deleteButton.on('click', deleteListItem);
 
     //3(ii). Explanation: Adding the class 'delete' (display: none) from the css:
     function deleteListItem() {
-        li.classList.add('delete');
+        li.addClass('delete');
     }
 
     // 4. Reordering the items: 
